813db90de497f39d891e20791b70be96d828888c
davidearbola <davidearbola@gmail.com>
2024-06-28 21:09:32 +0200

    aggiunto link alla pagina about e alla pagina Home

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,10 @@
 <div class="my_box_container">
     <div class="row align-items-center">
         <div class="col-12 col-md-2 text-center text-md-start mb-3 mb-md-0 p-0">
-            <img src="{{ asset('images/dc-logo.png') }}" />
+            <a href="{{ url('/') }}"><img src="{{ asset('images/dc-logo.png') }}" /></a>
         </div>
-        <div class="col-12 col-md-10 p-0 gap-3 d-flex justify-content-end">
-            <a href="#">CHARACTERS</a>
+        <div id="linkMenu" class="col-12 col-md-10 p-0 gap-3 d-flex justify-content-end">
+            <a href="{{ url('/about') }}">ABOUT</a>
             <a href="#">COMICS</a>
             <a href="#">MOVIES</a>
             <a href="#">TV</a>
