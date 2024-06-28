34151c1193d43ec50de1685b29d2c0b43dfa58a2
davidearbola <davidearbola@gmail.com>
2024-06-28 16:24:20 +0200

    aggiunto bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Laravel Comics</title>
 </head>
 <body>
-    <p>Questa è la Homepage</p>
-    <h1>{{ $comics[0]['title'] }}</h1>
-    <p>{{ $comics[0]['description'] }}</p>
-    <img src="{{ $comics[0]['thumb'] }}">
+        <p class="text-center">Questa è la Homepage</p>
+        <h1>{{ $comics[0]['title'] }}</h1>
+        <p>{{ $comics[0]['description'] }}</p>
+        <img src="{{ $comics[0]['thumb'] }}">
 </body>
 </html>
