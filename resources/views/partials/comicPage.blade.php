747b987e3d8a56b32158dc6169cb2a8ca58306f7
davidearbola <davidearbola@gmail.com>
2024-07-01 14:16:06 +0200

    minor fix style

--- resources/views/partials/comicPage.blade.php
+++ resources/views/partials/comicPage.blade.php
@@ -1,17 +1,11 @@
-<div class="full_box">
-    <div class="my_box_container py-5">
-        <div class="row">
-            <div class="col-5">
-                <img class="w-100" src="{{ $thumb }}">
-            </div>
-            <div class="col-7">
-                <h3>{{ $title }}</h3>
-                <p>{{ $description }}</p>
-                <p>Prezzo: {{ $price }}</p>
-                <span>Serie: {{ $series }}</span><br>
-                <span>Pubblicazione: {{ $sale_date }}</span><br>
-                <span>Categoria: {{ $type }}</span><br>
-            </div>
-        </div>
-    </div>
+<div class="col-2">
+    <img class="w-100" src="{{ $thumb }}">
+</div>
+<div class="col-10">
+    <h3>{{ $title }}</h3>
+    <p>{{ $description }}</p>
+    <p>Prezzo: {{ $price }}</p>
+    <span>Serie: {{ $series }}</span><br>
+    <span>Pubblicazione: {{ $sale_date }}</span><br>
+    <span>Categoria: {{ $type }}</span><br>
 </div>
